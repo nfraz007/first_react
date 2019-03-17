c4e83fd897efc159f9943e73fad4414a055ea35e
Nazish Fraz <nazish@mildlyclassic.com>
2019-03-17 14:12:50 +0530

    Updates

---
+++ precache-manifest.8b9d634bf09568c0bacc5252973e407c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f0f43ffb039dcfac62c",
+    "url": "/first_react/static/js/runtime~main.f20f8be8.js"
+  },
+  {
+    "revision": "8edd04247519dc268eeb",
+    "url": "/first_react/static/js/main.47f9a3e2.chunk.js"
+  },
+  {
+    "revision": "e89b6db1e14376caaecc",
+    "url": "/first_react/static/js/2.dd627044.chunk.js"
+  },
+  {
+    "revision": "1895bfb1c9493c06f2a3ab06f11019c0",
+    "url": "/first_react/index.html"
+  }
+];
